5c5ce4f06398d922b6238f681898006990e2c4f0
Gautam Singh <5769869+gautamsi@users.noreply.github.com>
2022-10-23 02:58:25 +0530

    add updateMany and list components

--- examples/basic/admin/pages/posts/index.tsx
+++ examples/basic/admin/pages/posts/index.tsx
@@ -1,3 +1,62 @@
-import { getListPage } from '@k6js/admin-ui/pages/ListPage';
+/** @jsxRuntime classic */
+/** @jsx jsx */
 
-export default getListPage({ listKey: 'Post' });
+import { jsx } from '@keystone-ui/core';
+import { ListPageComponents } from '@k6js/admin-ui';
+import { getListPage } from '@k6js/admin-ui/pages/ListPage';
+import { Button } from '@keystone-ui/button';
+import { Fragment } from 'react';
+
+const components: ListPageComponents = {
+  Header: ({ listKey }) => <div>List Header Custom - {listKey}</div>,
+  Actions: ({}) => (
+    <Fragment>
+      <Button tone="active">Action 1</Button>
+      <Button tone="active">Action 2</Button>
+      <Button tone="active">Action 3</Button>
+      <Button tone="active">Action 4</Button>
+      <Button tone="active">Action 5</Button>
+      <Button tone="active">Action 6</Button>
+      <Button tone="active">Action 7</Button>
+      <Button tone="active">Action 8</Button>
+      <Button tone="active">Action 9</Button>
+      <Button tone="active">Action 10</Button>
+      <Button tone="active">Action 11</Button>
+      <Button tone="active">Action 12</Button>
+      <Button tone="active">Action 13</Button>
+      <Button tone="active">Action 14</Button>
+      <Button tone="active">Action 15</Button>
+      <Button tone="active">Action 16</Button>
+      <Button tone="active">Action 17</Button>
+      <Button tone="active">Action 18</Button>
+      <Button tone="active">Action 19</Button>
+      <Button tone="active">Action 20</Button>
+    </Fragment>
+  ),
+  SelectionActions: ({ list, refetch, selectedItems }) => (
+    <Fragment>
+      <Button tone="active">Action 1</Button>
+      <Button tone="active">Action 2</Button>
+      <Button tone="active">Action 3</Button>
+      <Button tone="active">Action 4</Button>
+      <Button tone="active">Action 5</Button>
+      <Button tone="active">Action 6</Button>
+      <Button tone="active">Action 7</Button>
+      <Button tone="active">Action 8</Button>
+      <Button tone="active">Action 9</Button>
+      <Button tone="active">Action 10</Button>
+      <Button tone="active">Action 11</Button>
+      <Button tone="active">Action 12</Button>
+      <Button tone="active">Action 13</Button>
+      <Button tone="active">Action 14</Button>
+      <Button tone="active">Action 15</Button>
+      <Button tone="active">Action 16</Button>
+      <Button tone="active">Action 17</Button>
+      <Button tone="active">Action 18</Button>
+      <Button tone="active">Action 19</Button>
+      <Button tone="active">Action 20</Button>
+    </Fragment>
+  ),
+};
+
+export default getListPage({ listKey: 'Post', components });
--- packages/admin-ui/src/types.ts
+++ packages/admin-ui/src/types.ts
@@ -1,11 +1,25 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 
+import { ListMeta } from '@keystone-6/core/types';
 import { FunctionComponent } from 'react';
 import { ItemData } from './utils';
 
 export type ItemPageComponents = {
-  Header?: FunctionComponent<{ listKey: string; item: ItemData }>;
+  Header?: FunctionComponent<{ listKey: string; item: ItemData; label: string }>;
   Sidebar?: FunctionComponent<{ listKey: string; item: ItemData }>;
   Actions?: FunctionComponent<{ listKey: string; item: ItemData }>;
+};
+
+export type ListPageComponents = {
+  Header?: FunctionComponent<{ listKey: string }>;
+  Actions?: FunctionComponent<{
+    listKey: string;
+    refetch: () => void;
+  }>;
+  SelectionActions?: FunctionComponent<{
+    list: ListMeta;
+    selectedItems: ReadonlySet<string>;
+    refetch: () => void;
+  }>;
 };
