3a74f4170381228e9d60ad3f0a4a660879f66208
Gautam Singh <5769869+gautamsi@users.noreply.github.com>
2025-03-31 06:29:04 +0530

    cleanup components

--- packages/admin-ui/src/components/SignoutButton.tsx
+++ packages/admin-ui/src/components/SignoutButton.tsx
@@ -1,25 +1,33 @@
-import { Button } from '@keystone-ui/button'
-import { type ReactNode, useEffect } from 'react'
+import { useEffect } from 'react'
 import { useMutation, gql } from '@keystone-6/core/admin-ui/apollo'
+import { useRouter } from '@keystone-6/core/admin-ui/router'
+import { ActionButton } from '@keystar/ui/button'
+import { Tooltip, TooltipTrigger } from '@keystar/ui/tooltip'
+import { Text } from '@keystar/ui/typography'
 
 const END_SESSION = gql`
-  mutation EndSession {
+  mutation KsAuthEndSession {
     endSession
   }
 `
 
-function SignoutButton({ children }: { children?: ReactNode }) {
-  const [endSession, { loading, data }] = useMutation(END_SESSION)
+export function SignoutButton({ authItemLabel }: { authItemLabel: string }) {
+  const router = useRouter()
+  const [endSession, { data }] = useMutation(END_SESSION)
   useEffect(() => {
     if (data?.endSession) {
-      window.location.reload()
+      router.push('/signin')
     }
   }, [data])
 
   return (
-    <Button size="small" isLoading={loading} onClick={() => endSession()}>
-      {children || 'Sign out'}
-    </Button>
+    <TooltipTrigger>
+      <ActionButton onPress={() => endSession()}>Sign out</ActionButton>
+      <Tooltip>
+        <Text>
+          Signed in as <strong>{authItemLabel}</strong>
+        </Text>
+      </Tooltip>
+    </TooltipTrigger>
   )
 }
-export { SignoutButton }
